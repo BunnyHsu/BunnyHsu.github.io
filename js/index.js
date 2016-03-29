3d9177728f27ecb6f1a18a820a1af9de84445bf7
bunny <179346696@qq.com>
2016-03-29 15:58:13 +0800

    March 29 2016

---
+++ js/index.js
@@ -0,0 +1,30 @@
+var heroShinker = function() {
+    var hero = $('.hero-nav'),
+        heroHeight = $('.hero-nav').outerHeight(true);
+        $(hero).parent().css('padding-top', heroHeight);
+   // when clicking items , decreasing the height of hero
+    $("#s2").click(function () {
+        $(hero).css('height', (100));
+    })
+    $("#s3").click(function () {
+        $(hero).css('height', (100));
+    })
+    $("#s4").click(function () {
+        $(hero).css('height', (100));
+    })
+
+    $(window).scroll(function() {
+        var scrollOffset = $(window).scrollTop();
+        if (scrollOffset < heroHeight) {
+            $(hero).css('height', (heroHeight - scrollOffset));
+        }
+        if (scrollOffset > (heroHeight - 215)) {
+            hero.addClass('fixme');
+        }
+        else {
+            hero.removeClass('fixme');
+        };
+    });
+}
+
+heroShinker();
